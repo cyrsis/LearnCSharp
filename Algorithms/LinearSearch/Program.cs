79e2184b4de64637fb1770c75eaa52a407e97b06
Victor <em3888@gmail.com>
2017-08-17 17:12:18 +0800

    Add some demo for Search

---
+++ Algorithms/LinearSearch/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using static System.Console;
+
+namespace LinearSearch
+{
+    class Program
+    {
+        public static void Main()
+        {
+            List<string> actors = new List<string>();
+
+            actors.Add("Tom Cruise");
+            actors.Add("Madonna");
+            actors.Add("Bruce Lee");
+            actors.Add("Jimmy Fallon");
+
+            WriteLine("Linear search results: \n\nIn the collection, there are {0} actors:", actors.Count);
+            PrintOut(actors);
+
+            WriteLine("\nOf which, the IndexOf(\"Madonna\") is {0}",
+                actors.IndexOf("Madonna"));
+
+        }
+
+        private static void PrintOut(List<string> cs)
+        {
+            WriteLine();
+            foreach (string item in cs)
+            {
+                WriteLine(item);
+            }
+        }
+    }
+}
